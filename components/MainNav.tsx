a12dbb7dec0c12260f996892d670a4aef35c7a2d
Christian Mack <sfcamack@icloud.com>
2023-11-27 18:33:03 -0500

    categories functionality implemented

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -21,7 +21,13 @@ export function MainNav({
       href: `/${params.storeId}/billboards`,
       label: "Billboards",
       active: pathname === `/${params.storeId}/billboards`,
-    },{
+    },
+    {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      active: pathname === `/${params.storeId}/categories`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
